6e5a1c4fe908837f46ddb459ca1119dab6e0344a
AlexKaracaoglu <karacaog@bc.edu>
2018-08-31 18:52:42 -0400

    initial commit

---
+++ src/main/java/com/recreation/recreation/RecreationApplication.java
@@ -0,0 +1,12 @@
+package com.recreation.recreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecreationApplication.class, args);
+	}
+}
