3958c4c8bfe21695a65c8bec14109cd9c716f05d
AlexKaracaoglu <karacaog@bc.edu>
2018-09-04 00:13:28 -0400

    #7 DeleteRegistration controller, dto, and service setup. Fixes of javadoc

---
+++ src/main/java/com/recreation/recreation/services/DeleteRegistrationService.java
@@ -0,0 +1,23 @@
+/*
+ * Property of 2150 Room 201
+ * 2018
+ */
+
+package com.recreation.recreation.services;
+
+import com.recreation.recreation.dtos.DeleteRegistrationDto;
+import org.springframework.stereotype.Service;
+
+/**
+ * DeleteRegistrationService
+ * @author Alex Karacaoglu
+ */
+
+@Service
+public class DeleteRegistrationService {
+
+    public void deleteRegistration(DeleteRegistrationDto deleteRegistrationDto) {
+
+    }
+
+}
