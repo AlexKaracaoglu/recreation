c75664ebac84f7fc5519b36c34295caae5402bfd
AlexKaracaoglu <karacaog@bc.edu>
2018-09-09 22:06:29 -0400

    #35 Update participant-info function name

--- src/main/java/com/recreation/recreation/controllers/GetParticipantInformationController.java
+++ src/main/java/com/recreation/recreation/controllers/GetParticipantInformationController.java
@@ -27,7 +27,7 @@ public class GetParticipantInformationController {
 
     // Example url: http://localhost:8080/participant-info?eagleId=3
     @RequestMapping(value = "/participant-info", method = RequestMethod.GET)
-    public ResponseEntity<ParticipantInformationDto> register(@RequestParam String eagleId) {
+    public ResponseEntity<ParticipantInformationDto> getParticipantInfo(@RequestParam String eagleId) {
         ParticipantInformationDto participant = participantInformationService.getParticipant(eagleId);
         return ResponseEntity.ok(participant);
     }
