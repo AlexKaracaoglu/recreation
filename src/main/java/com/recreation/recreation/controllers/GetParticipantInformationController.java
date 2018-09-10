d1a858038d7daac7574ee5fddd701c3268a6263a
AlexKaracaoglu <karacaog@bc.edu>
2018-09-09 22:04:35 -0400

    #35 Update participant-info controller name

--- src/main/java/com/recreation/recreation/controllers/GetParticipantInformationController.java
+++ src/main/java/com/recreation/recreation/controllers/GetParticipantInformationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 
 @Controller
-public class GetParticipantController {
+public class GetParticipantInformationController {
 
     @Autowired
     GetParticipantInformationService participantInformationService;
